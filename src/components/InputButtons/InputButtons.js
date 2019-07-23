0947bf9cec6dbc002181f5e9a2180d1c240be9dc
bardamuffin <bardamuffin@gmail.com>
2019-07-23 19:31:51 +0200

    f

--- src/components/InputButtons/InputButtons.js
+++ src/components/InputButtons/InputButtons.js
@@ -1,17 +1,61 @@
-import React from 'React';
+import React, { Component } from 'React';
 import { StyleSheet, View } from 'react-native';
 import { Input } from 'react-native-elements';
 
 //extract css properties https://facebook.github.io/react-native/docs/stylesheet.html#flatten
 
-const InputButtons = (props) => {
-    return <View style={styles.container}>
-        <Input label={props.boxStyle.borderTopLeftRadius} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='topLeft'></Input>
-        <Input label={props.boxStyle.borderTopRightRadius} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='topRight'></Input>
-        <Input label={props.boxStyle.borderBottomLeftRadius} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='bottLeft'></Input>
-        <Input label={props.boxStyle.borderBottomRightRadius} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='bottRight'></Input>
-    </View>
+export default class InputButtons extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            style : props.boxStyle
+        }
+    }
+    objToString = obj => {
+        let val = '';
+        for (let property in obj ) {
+            val += ('"' + property+ '"' + " " + " : " + obj[property] + ", ")
+        }
+        return val;
+    }
+
+    handleModifiedValue(val, id) {
+        //TODO: input validation
+        switch(id) {
+            case 'btlr':
+                console.log('btlr');
+                this.setState({
+                    borderTopLeftRadius: val
+                })
+                break;
+            case 'btrr':
+                console.log('btrr');
+                break;
+            case 'bblr':
+                break;
+            case 'bbrr':
+                break;            
+        }
+    }
+    componentDidUpdate() {
+        console.log("Did update: " + this.props.boxStyle)
+    }
+    componentWillMount() {
+        console.log("Will! : " + this.props.boxStyle.borderBottomLeftRadius)
+    }
+    render() {
+        return(
+            <View style={styles.container}>
+                <Input value={this.state.style.borderTopLeftRadius.toString()} label={this.props.boxStyle.borderTopLeftRadius} onChangeText={(val) => this.handleModifiedValue(val, "btlr")} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='topLeft'></Input>
+                <Input value={this.props.boxStyle.borderTopRightRadius.toString()} label={this.props.boxStyle.borderTopRightRadius} onChangeText={(val) => this.handleModifiedValue(val, "btrr")} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='topRight'></Input>
+                <Input value={this.props.boxStyle.borderBottomLeftRadius.toString()} label={this.props.boxStyle.borderBottomLeftRadius} onChangeText={(val) => this.handleModifiedValue(val, "bblr")} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='bottLeft'></Input>
+                <Input value={this.props.boxStyle.borderBottomRightRadius.toString()} label={this.props.boxStyle.borderBottomRightRadius} onChangeText={(val) => this.handleModifiedValue(val, "bbrr")} containerStyle={styles.input} inputContainerStyle={styles.text} placeholder ='bottRight'></Input>
+            </View>
+        );    
+    }
 }
+            
+
 
 const styles = StyleSheet.create({
     container: {
@@ -27,12 +71,9 @@ const styles = StyleSheet.create({
     text: {
         flexDirection: "column",
         alignItems: "center",
+    },
+    inputContainerStyle: {
+        justifyContent: "center"
     }
 });
-
-
-export default InputButtons;
-
-        
-        
-        
+       
